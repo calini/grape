b72b19df4b5b91e2145a292b187c61cf005bde21
Calin Ilie <calin@ilie.io>
2019-04-03 01:00:10 +0100

    Cleanup

--- makefile
+++ makefile
@@ -1,28 +1,8 @@
 build:
 	@go build -o out/grape
 
-run-test-dict:
-	@out/grape \
-		-url https://github.com/%s \
-		-dict junk/dicts/random.txt \
-		-query ".p-name .avatar" \
-		-output out/output_github.csv
+install:
+	@go install
 
-run-test-iter:
-	@out/grape \
-		-url https://github.com/%d \
-		-from 1 \
-		-to 10 \
-		-query ".p-name .avatar" \
-		-output out/output_iter.csv
-
-run-test-json-TODO:
-	@out/grape \
-		-json
-		-url https://github.com/%d \
-		-from 1 \
-		-to 10 \
-		-query ".p-name .avatar" \
-		-output out/output_iter.csv
-
-
+test:
+	@go test ./...
--- scraper/fetch.go
+++ scraper/fetch.go
@@ -8,9 +8,9 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-const ATTRIBUTE_SEPARATOR = "§"
+const AttributeSeparator = "§"
 
-// Fetch returns a list of rsults parsed from the link corresponding to the token
+// Fetch returns a list of results parsed from the link corresponding to the token
 func Fetch(url string, queries []string) ([]string, error) {
 	res, err := http.Get(url)
 	if err != nil {
@@ -26,7 +26,7 @@ func Fetch(url string, queries []string) ([]string, error) {
 		return nil, fmt.Errorf("bad response from server: %s", res.Status)
 	}
 
-	// parse body with goquery.
+	// parse body with GOQuery.
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse page: %v", err)
@@ -36,10 +36,10 @@ func Fetch(url string, queries []string) ([]string, error) {
 	var r []string
 	for _, q := range queries {
 		var result string
-		if !strings.Contains(q, ATTRIBUTE_SEPARATOR) {
+		if !strings.Contains(q, AttributeSeparator) {
 			result = doc.Find(q).Text()
 		} else {
-			query := strings.Split(q, ATTRIBUTE_SEPARATOR)
+			query := strings.Split(q, AttributeSeparator)
 			result, _ = doc.Find(query[0]).Attr(query[1])
 		}
 		r = append(r, strings.TrimSpace(result))
