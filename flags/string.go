f81cf21ab351ec465e6d3a655732dc0841aa2625
Calin Ilie <calin@ilie.io>
2019-05-20 15:50:31 +0100

    Update string.go

--- flags/string.go
+++ flags/string.go
@@ -1,20 +1,24 @@
 package flags
 
+// String represents a string flag
 type String struct {
 	set   bool
 	Value string
 }
 
+// Set sets the flag
 func (sf *String) Set(x string) error {
 	sf.Value = x
 	sf.set = true
 	return nil
 }
 
+// IsSet checks if the flag is set
 func (sf *String) IsSet() bool {
 	return sf.set
 }
 
+// String returns the flag value
 func (sf *String) String() string {
 	return sf.Value
 }
