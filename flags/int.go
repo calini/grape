a6be9537ebfbb21d171afdd0dd97c90f9359fc43
Calin Ilie <calin@ilie.io>
2019-05-20 15:49:59 +0100

    Update int.go

--- flags/int.go
+++ flags/int.go
@@ -2,21 +2,25 @@ package flags
 
 import "strconv"
 
+// Int represents an integer flag
 type Int struct {
 	set   bool
 	Value int
 }
 
+// Set sets the flag
 func (sf *Int) Set(x string) error {
 	sf.Value, _ = strconv.Atoi(x)
 	sf.set = true
 	return nil
 }
 
+// IsSet checks if the flag is set
 func (sf *Int) IsSet() bool {
 	return sf.set
 }
 
+// String returns the value as a string
 func (sf *Int) String() string {
 	return string(sf.Value)
 }
